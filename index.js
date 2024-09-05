362fca49335516930fa29e669ec1a78574471a55
Shane A. Healy <ShaneAHealy55@gmail.com>
2024-09-04 23:32:59 -0500

    Added PseudoCode

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+/*PseudoCode: (Contained in JavaScript file)
+
+1.) Write PseudoCode elaborating all steps, in order to create a clear thought process. Code is mostly about thinking how to solve puzzles, and breaking complex tasks into simplified components, then arranging those small steps into an efficient sequence of vertically-integrated, self-contained steps. The actual typing is muscle memory, or Googling for references of code, which is (in some respects) the least important part of the process. 
+2.) Create a new GitHub repository
+3.) Create a folder on Desktop
+4.) Enter Git Bash, cd into terminal of the newly-created folder. Git clone the SSH key code from GitHub into this folder.
+5.) Open VSCode. Open the newly-created folder which git cloned the new, empty git repository.  
+6.) Create an HTML file and a JavaScript file in VS Code folder.
+7.) Create an HTML template to inject the JavaScript code into, using the <script> tag.
+8.) In the external JavaScript file, create a string that says, “You have received this message because you have been chosen to open an important vault. Here is the secret combination:”
+9.) [Mostly repeating initial instructions in rephrased terms from this point.] 
+10.) Define three variables in JavaScript, using the “const” keyword.
+11.) Express one multiplication, one addition, and one subtraction operation related to the three variables defined in the previous step. Let the results of these mathematical operations equal “10”, “40”, and “39”, respectively. 
+12.) Throughout code, add comments with explanations for strangers who are randomly dropping in on the code with no context, and therefore need simple explanations to save them time and accelerate their evaluation. In JavaScript, these comments can be single-line “//” or multi-line bracketing the content “/* [insert huge block of code] */ /*”. In HTML, these comments will bracket the content “<!--  [insert huge block of code]  -->”. 
+13.) Create some form of alert to reward the user with the vault code. Multiple options exist here, depending on whether to use a generic or narrowly-specialized option. 
+14.) Submit finished GitHub URL to the Canvas project 
+
+
+End of PseudoCode Above, actual code begins Below*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
